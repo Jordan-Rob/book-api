d1349e50bc791788baa4dd909a5be5ddbae9b75d
Jordan <jordanrob709@gmail.com>
2022-10-07 13:04:45 +0300

    added an Input class to map client data to our Book Model, Created a Mutation class for Create funtionality

--- api/schema.py
+++ api/schema.py
@@ -20,3 +20,32 @@ class Query(graphene.ObjectType):
 
     def resolve_book(self, info, book_id):
         return Book.objects.get(pk=book_id)
+
+
+
+class BookInput(graphene.InputObjectType):
+    id = graphene.ID()
+    title = graphene.String()
+    author = graphene.String()
+    year_published = graphene.String()
+    review = graphene.Int()
+
+
+
+class CreateBook(graphene.Mutation):
+    class Arguments:
+        book_data = BookInput(required=True)
+
+    book = graphene.Field(BookType)
+
+    @staticmethod
+    def mutate(root, info, book_data=None):
+        book_instance = Book(
+            title = book_data.title,
+            author = book_data.author,
+            year_published = book_data.year_published,
+            review = book_data.review
+        )
+
+        book_instance.save()
+        return CreateBook(book=book_instance)
